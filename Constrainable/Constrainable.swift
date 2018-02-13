46c015863dcef47259eefd2d2774440a0c00a858
simone <simone.c@lumyer.com>
2018-02-13 22:51:57 +0100

    Added priority

--- Constrainable/Constrainable.swift
+++ Constrainable/Constrainable.swift
@@ -35,38 +35,44 @@ public enum ConstraintRelationship {
     case equal, lessThanOrEqual, greaterThanOrEqual
 }
 
-public func constraint<Anchor, Axis>(_ originKeyPath: KeyPath<Constrainable, Anchor>, to destinationKeyPath: KeyPath<Constrainable, Anchor>, of destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1)-> Constraint where Anchor: NSLayoutAnchor<Axis> {
+public func constraint<Anchor, Axis>(_ originKeyPath: KeyPath<Constrainable, Anchor>, to destinationKeyPath: KeyPath<Constrainable, Anchor>, of destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1, priority: UILayoutPriority = .required) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
     return { constrainable in
+        let constraint: NSLayoutConstraint
         switch relationship {
         case .equal:
-            return constrainable[keyPath: originKeyPath].constraint(equalTo: destination[keyPath: destinationKeyPath], constant: offset).setMultiplier(multiplier)
+            constraint = constrainable[keyPath: originKeyPath].constraint(equalTo: destination[keyPath: destinationKeyPath], constant: offset).withMultiplier(multiplier)
         case .lessThanOrEqual:
-            return constrainable[keyPath: originKeyPath].constraint(lessThanOrEqualTo: destination[keyPath: destinationKeyPath], constant: offset).setMultiplier(multiplier)
+            constraint =  constrainable[keyPath: originKeyPath].constraint(lessThanOrEqualTo: destination[keyPath: destinationKeyPath], constant: offset).withMultiplier(multiplier)
         case .greaterThanOrEqual:
-            return constrainable[keyPath: originKeyPath].constraint(greaterThanOrEqualTo: destination[keyPath: destinationKeyPath], constant: offset).setMultiplier(multiplier)
+            constraint =  constrainable[keyPath: originKeyPath].constraint(greaterThanOrEqualTo: destination[keyPath: destinationKeyPath], constant: offset).withMultiplier(multiplier)
         }
+        constraint.priority = priority
+        return constraint
     }
 }
 
-public func constraint<Anchor, Axis>(same keyPath: KeyPath<Constrainable, Anchor>, as destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
-    return constraint(keyPath, to: keyPath, of: destination, relationship: relationship, offset: offset, multiplier: multiplier)
+public func constraint<Anchor, Axis>(same keyPath: KeyPath<Constrainable, Anchor>, as destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1, priority: UILayoutPriority = .required) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
+    return constraint(keyPath, to: keyPath, of: destination, relationship: relationship, offset: offset, multiplier: multiplier, priority: priority)
 }
 
-public func constraint<LayoutDimension>(_ originKeyPath: KeyPath<Constrainable, LayoutDimension>, to destinationKeyPath: KeyPath<Constrainable, LayoutDimension>, of destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1) -> Constraint where LayoutDimension: NSLayoutDimension {
+public func constraint<LayoutDimension>(_ originKeyPath: KeyPath<Constrainable, LayoutDimension>, to destinationKeyPath: KeyPath<Constrainable, LayoutDimension>, of destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1, priority: UILayoutPriority = .required) -> Constraint where LayoutDimension: NSLayoutDimension {
     return { constrainable in
+        let constraint: NSLayoutConstraint
         switch relationship {
         case .equal:
-            return constrainable[keyPath: originKeyPath].constraint(equalTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
+            constraint = constrainable[keyPath: originKeyPath].constraint(equalTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
         case .lessThanOrEqual:
-            return constrainable[keyPath: originKeyPath].constraint(lessThanOrEqualTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
+            constraint = constrainable[keyPath: originKeyPath].constraint(lessThanOrEqualTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
         case .greaterThanOrEqual:
-            return constrainable[keyPath: originKeyPath].constraint(greaterThanOrEqualTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
+            constraint = constrainable[keyPath: originKeyPath].constraint(greaterThanOrEqualTo: destination[keyPath: destinationKeyPath], multiplier: multiplier, constant: offset)
         }
+        constraint.priority = priority
+        return constraint
     }
 }
 
-public func constraint<LayoutDimension>(same keyPath: KeyPath<Constrainable, LayoutDimension>, as destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1) -> Constraint where LayoutDimension: NSLayoutDimension {
-    return constraint(keyPath, to: keyPath, of: destination, relationship: relationship, offset: offset, multiplier: multiplier)
+public func constraint<LayoutDimension>(same keyPath: KeyPath<Constrainable, LayoutDimension>, as destination: Constrainable, relationship: ConstraintRelationship = .equal, offset: CGFloat = 0, multiplier: CGFloat = 1, priority: UILayoutPriority = .required) -> Constraint where LayoutDimension: NSLayoutDimension {
+    return constraint(keyPath, to: keyPath, of: destination, relationship: relationship, offset: offset, multiplier: multiplier, priority: priority)
 }
 
 public func constraint<LayoutDimension>(_ keyPath: KeyPath<Constrainable, LayoutDimension>, to constant: CGFloat) -> Constraint where LayoutDimension: NSLayoutDimension {
@@ -75,7 +81,7 @@ public func constraint<LayoutDimension>(_ keyPath: KeyPath<Constrainable, Layout
     }
 }
 
-public func sameEdges(as destination: Constrainable, with insets: UIEdgeInsets = .zero) -> [Constraint] {
+public func constraint(edgesTo destination: Constrainable, with insets: UIEdgeInsets = .zero) -> [Constraint] {
 
     let top: Constraint = { constrainable  in
         return constrainable.topAnchor.constraint(equalTo: destination.topAnchor, constant: insets.top)
